b8a819f5c7c4a020e635bf1b51ac60a58ad4a5ba
shomil03 <shomil02020@gmail.com>
2024-01-11 14:35:17 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Shomil Singh on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
